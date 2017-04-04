92e529233bddbc19009e7ca93a112b38ddb59474
Lukasz Kolacz <lukaszkolacz15@gmail.com>
2017-04-04 14:41:35 +0200

    add task2 uncomplite

--- src/pl/akademiakodu/Main.java
+++ src/pl/akademiakodu/Main.java
@@ -5,12 +5,17 @@ import pl.akademiakodu.task1.Zarowka;
 public class Main {
 
     public static void main(String[] args) {
-	// task1
+//	 task1
 
 //        Zarowka newZarowkaWithParam = new Zarowka(5, 15);
 //        newZarowkaWithParam.getZywotnoscAndMod();
+//
+//        Zarowka newZarowka = new Zarowka();
+//        newZarowka.getZywotnoscAndMod();
 
-        Zarowka newZarowka = new Zarowka();
-        newZarowka.getZywotnoscAndMod();
+//   task2
+
+
+
     }
 }
---
+++ src/pl/akademiakodu/task2/Piosenka.java
@@ -0,0 +1,34 @@
+package pl.akademiakodu.task2;
+
+/**
+ * Created by Lukasz Kolacz on 04.04.2017.
+ */
+public class Piosenka {
+
+    private String nazwa;
+    private String artysta;
+
+    public Piosenka(String nazwa, String artysta) {
+        this.nazwa = nazwa;
+        this.artysta = artysta;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public String getArtysta() {
+        return artysta;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public void setArtysta(String artysta) {
+        this.artysta = artysta;
+    }
+
+
+
+}
